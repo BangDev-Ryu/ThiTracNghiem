daca0b852cf80d2d1b881395eefe766aa8c68c93
minh <tranminhnt2@gmail.com>
2025-03-20 04:25:27 +0700

    sql

--- tracnghiem.sql
+++ tracnghiem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2025 at 10:17 PM
+-- Generation Time: Mar 19, 2025 at 10:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
